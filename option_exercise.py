e4f7400167e1f838b93c352611cf8a83afa7193e
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-10-01 18:55:25 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 
 st.sidebar.markdown("<h4 style='text-align: center; font-size:18px; margin-bottom: 0;'>Client option exercise approach</h4>", unsafe_allow_html=True)
-st.sidebar.selectbox("",("Random scatter","Pro rata"))
+add_selectbox = st.sidebar.selectbox("",("Random scatter","Pro rata"))
 
 qccp_margins = pd.DataFrame({'SYMBOL':['Future', 'Call', 'Put'], 'LONG':[1000, 180, 130], 'SHORT':[950, 190, 145]})
 qccp_margins = qccp_margins.set_index('SYMBOL')
