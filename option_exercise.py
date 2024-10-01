1928aee3aea832b897d0ee9671eb432b64fc1009
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-10-01 19:33:22 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -11,12 +11,7 @@ import numpy as np
 st.sidebar.markdown("<h4 style='text-align: center; font-size:18px; margin-bottom: -200px;'>Client option exercise approach</h4>", unsafe_allow_html=True)
 st.session_state['method'] = st.sidebar.selectbox("",("Pro rata","Random scatter"),index=0)
 
-st.sidebar.markdown("<p style='text-align: center; margin-bottom: -30px;'font-size:18px;'>Intstrument attributes</p>", unsafe_allow_html=True)
-instruments = pd.DataFrame({'SYMBOL':['Opt1', 'Opt2', 'Opt3', 'Opt4'], 'Type':['Call', 'Call', 'Put','Put'], 'Contract multiplier':[1000, 1000, 1000,1000]})
-instruments = instruments.set_index('SYMBOL')
-st.session_state['instruments'] = st.sidebar.data_editor(instruments, disabled=('SYMBOL'), use_container_width=True)
-
-st.sidebar.markdown("<p style='text-align: center;'font-size:18px;'>Prices</p>", unsafe_allow_html=True)
+st.sidebar.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Intstrument attributes</p>", unsafe_allow_html=True)
 instruments = pd.DataFrame({'SYMBOL':['Opt1', 'Opt2', 'Opt3', 'Opt4'], 'Type':['Call', 'Call', 'Put','Put'], 'Contract multiplier':[1000, 1000, 1000,1000]})
 instruments = instruments.set_index('SYMBOL')
 st.session_state['instruments'] = st.sidebar.data_editor(instruments, disabled=('SYMBOL'), use_container_width=True)
