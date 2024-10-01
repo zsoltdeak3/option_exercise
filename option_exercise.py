2ce02c9a133347a6e5f9e493e5da96fb6611b405
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-10-01 18:16:30 +0300

    Add files via upload

---
+++ option_exercise.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 20:30:40 2024
+
+@author: zsolt
+"""
+
+import streamlit as st
+
+st.title('Hello, Streamlit!')
+st.write('This is a simple Streamlit app deployed from GitHub.')
