73428f382a5f55cc15245883cb4310bdb58d3abd
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-10-01 18:32:38 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -7,5 +7,12 @@ Created on Mon Sep 30 20:30:40 2024
 
 import streamlit as st
 
-st.title('Hello, Streamlit!')
-st.write('This is a simple Streamlit app deployed from GitHub.')
+st.sidebar.markdown("<p style='text-align: center;'font-size:18px;'>IM_SINGLE_POSITION - QCCP</p>", unsafe_allow_html=True)
+qccp_margins = pd.DataFrame({'SYMBOL':['Future', 'Call', 'Put'], 'LONG':[1000, 180, 130], 'SHORT':[950, 190, 145]})
+qccp_margins = qccp_margins.set_index('SYMBOL')
+st.session_state['qccp_margins'] = st.sidebar.data_editor(qccp_margins, disabled=('SYMBOL'), use_container_width=True)
+st.sidebar.markdown("<p style='text-align: center;'font-size:18px;'>ITD THEORETICAL PRICES - MITCH</p>", unsafe_allow_html=True)
+theor_prices = pd.DataFrame({'SYMBOL':['Future', 'Call', 'Put'], 'THEORETICAL PRICE':[9500., 5.3, 3.2],
+                            'CONTRACT SIZE':[1000, 5, 5]})
+theor_prices = theor_prices.set_index('SYMBOL')
+st.session_state['theor_prices'] = st.sidebar.data_editor(theor_prices, disabled=('SYMBOL', 'CONTRACT SIZE'))
