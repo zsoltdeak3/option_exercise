46619ce38ee3abef447ede6819dda5d3d058f10d
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-10-01 19:17:36 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -14,7 +14,7 @@ add_selectbox = st.sidebar.selectbox("",("Pro rata","Random scatter"),index=0)
 st.sidebar.markdown("<p style='text-align: center;'font-size:18px;'>Intstrument attributes</p>", unsafe_allow_html=True)
 instruments = pd.DataFrame({'SYMBOL':['Opt1', 'Opt2', 'Opt3', 'Opt4'], 'Type':['Call', 'Call', 'Put','Put'], 'Contract multiplier':[1000, 1000, 1000,1000]})
 instruments = intstruments.set_index('SYMBOL')
-st.session_state['instruments'] = st.sidebar.data_editor(instruments, disabled=True, use_container_width=True)
+st.session_state['instruments'] = st.sidebar.data_editor(instruments, disabled=('SYMBOL'), use_container_width=True)
 
 st.sidebar.markdown("<p style='text-align: center;'font-size:18px;'>ITD THEORETICAL PRICES - MITCH</p>", unsafe_allow_html=True)
 theor_prices = pd.DataFrame({'SYMBOL':['Future', 'Call', 'Put'], 'THEORETICAL PRICE':[9500., 5.3, 3.2],
