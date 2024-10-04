a9352174066df8f81a577edffd82fe6979f7c62a
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-10-04 18:43:08 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -45,5 +45,7 @@ st.session_state['edsp'] = col2.data_editor(edsp, disabled=('SYMBOL'), use_conta
 
 #Client positions
 client_pos = pd.DataFrame({'Client':['Client1', 'Client2', 'Client3', 'Client4'], 'SYMBOL':['Opt1', 'Opt1', 'Opt1', 'Opt1'], 'Net position':[10, 10, -15,-12]})
+st.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Client Positions</p>", unsafe_allow_html=True)
+st.session_state['client_pos'] = st.data_editor(client_pos, disabled=(['SYMBOL','Client']), use_container_width=True)
 
 
