ee3ffc507922742cf7ee75c1994a981f8f5bcaa6
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-10-04 20:22:26 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -28,6 +28,35 @@ if st.session_state['example'] == "Single instrument":
   st.session_state['method'] = st.sidebar.selectbox("Client option exercise approach",("Pro rata","Random scatter"),index=0)
   
   threshold = st.sidebar.number_input('Exercise threshold (%)',min_value=0.0, value=1.0, step=0.1,format="%.1f")
+
+  #First part left
+  st.sidebar.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Option Attributes</p>", unsafe_allow_html=True)
+  instruments = pd.DataFrame({'Attribute':['Symbol','Type','Contract size','Strike','Underlying'],["Value:['Opt1','Call',1000,500,'Und1',1200]})
+  instruments = instruments.set_index('Attribute')
+  st.session_state['instruments'] = col1.data_editor(instruments, disabled=('Attribute'), use_container_width=True)
+  
+  #First part right
+  col2.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Settlement prices</p>", unsafe_allow_html=True)
+  edsp = pd.DataFrame({'SYMBOL':['Und1'], 'EDSP':[1000]})
+  edsp = edsp.set_index('SYMBOL')
+  st.session_state['edsp'] = col2.data_editor(edsp, disabled=('SYMBOL'), use_container_width=True)
+  
+  #st.sidebar.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Settlement prices</p>", unsafe_allow_html=True)
+  #edsp = pd.DataFrame({'SYMBOL':['Und1'], 'EDSP':[1000]})
+  #edsp = edsp.set_index('SYMBOL')
+  #st.session_state['edsp'] = st.sidebar.data_editor(edsp, disabled=('SYMBOL'), use_container_width=True)
+  
+  #Client positions
+  c1, c2, c3 = st.columns([1,1,1])
+  client_pos = pd.DataFrame({'Client':['Client1', 'Client2', 'Client3', 'Client4'], 'SYMBOL':['Opt1', 'Opt1', 'Opt1', 'Opt1'], 'Net position':[10, 10, -15,-12]})
+  client_pos = client_pos.set_index('Client')
+  c2.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Client Positions</p>", unsafe_allow_html=True)
+  st.session_state['client_pos'] = c2.data_editor(client_pos, disabled=(['SYMBOL','Client']), use_container_width=True)
+else:
+  
+  st.session_state['method'] = st.sidebar.selectbox("Client option exercise approach",("Pro rata","Random scatter"),index=0)
+  
+  threshold = st.sidebar.number_input('Exercise threshold (%)',min_value=0.0, value=1.0, step=0.1,format="%.1f")
   
   col1, col2 = st.columns([2,1])
   #First part left
@@ -53,5 +82,3 @@ if st.session_state['example'] == "Single instrument":
   client_pos = client_pos.set_index('Client')
   c2.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Client Positions</p>", unsafe_allow_html=True)
   st.session_state['client_pos'] = c2.data_editor(client_pos, disabled=(['SYMBOL','Client']), use_container_width=True)
-else:
-  None
