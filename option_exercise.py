fdd3dd4fb362bb15f7a41d882e75697edda762c9
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-10-04 11:09:46 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -21,12 +21,12 @@ def moneyness (type,strike,edsp):
 st.sidebar.markdown("<h4 style='text-align: center; font-size:18px; margin-bottom: -200px;'>Client option exercise approach</h4>", unsafe_allow_html=True)
 st.session_state['method'] = st.sidebar.selectbox("",("Pro rata","Random scatter"),index=0)
 
+threshold = st.sidebar.number_input('Exercise threshold (%)', value=1.0, step=0.1,format="%.1f")
+
 st.sidebar.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Settlement prices</p>", unsafe_allow_html=True)
 edsp = pd.DataFrame({'SYMBOL':['Und1', 'Und2'], 'EDSP':[1000,1200]})
 edsp = edsp.set_index('SYMBOL')
 st.session_state['edsp'] = st.sidebar.data_editor(edsp, disabled=('SYMBOL'), use_container_width=True)
-
-threshold = st.sidebar.number_input('Exercise threshold', value=1.0, step=0.1,format="%.1f")
 
 st.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Intstrument attributes</p>", unsafe_allow_html=True)
 instruments = pd.DataFrame({'SYMBOL':['Opt1', 'Opt2', 'Opt3', 'Opt4'], 'Type':['Call', 'Call', 'Put','Put'], 'Contract size':[1000, 1000, 1000,1000], 'Strike':[500,400,300,200],'Underlying':['Und1','Und1','Und2','Und2']})
