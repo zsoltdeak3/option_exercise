93e2e238e8286aa75af39460792f51c568af7cc4
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-10-04 20:26:20 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -33,7 +33,7 @@ if st.session_state['example'] == "Single instrument":
   st.sidebar.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Option Attributes</p>", unsafe_allow_html=True)
   instrument = pd.DataFrame({'Attribute':['Symbol','Type','Contract size','Strike','Underlying', 'EDSP'],'Value':['Opt1','Call',1000,500,'Und1',1200]})
   instrument = instrument.set_index('Attribute')
-  st.session_state['instrument'] = col1.data_editor(instrument, disabled=('Attribute'), use_container_width=True)
+  st.session_state['instrument'] = st.data_editor(instrument, disabled=('Attribute'), use_container_width=True)
   
   #First part right
   st.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Settlement prices</p>", unsafe_allow_html=True)
