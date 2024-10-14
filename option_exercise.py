2160ab3168bcd518483f4316a3f593347d6e7bf3
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-10-14 18:40:24 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -45,7 +45,7 @@ if st.session_state['example'] == "Single instrument":
 
   #CCP position
   st.session_state['net_client_pos'] = client_pos['Net position'].sum()
-  ccp_pos = pd.DataFrame({'CCP account':['Net omnibus'], 'SYMBOL':['Opt1'], 'Net position': [st.session_state['net_client_pos']})
+  ccp_pos = pd.DataFrame({'CCP account':['Net omnibus'], 'SYMBOL':['Opt1'], 'Net position': [st.session_state['net_client_pos']]})
   ccp_pos = ccp_pos.set_index('CCP account')
   st.session_state['ccp_pos'] = c2.data_editor(ccp_pos,disabled=(['CCP account','SYMBOL','Net position']),use_container_width=True)
 
