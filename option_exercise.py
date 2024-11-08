277693b0f027356d44832f6a3dc01cbe44ae8826
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-11-08 13:16:03 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -37,7 +37,8 @@ if st.session_state['example'] == "Single instrument":
   if 'instrument' not in st.session_state:
     instrument = {'Attribute': ['Symbol', 'Type', 'Contract size', 'Strike', 'Underlying', 'EDSP'],
                   'Value': ['Opt1', 'Call', 1000, 500, 'Und1', 1200]}
-  st.session_state['instrument'] = st.sidebar.data_editor(pd.DataFrame(instrument).set_index('Attribute'), use_container_width=True)
+    st.session_state['instrument'] = pd.DataFrame(instrument)
+  st.session_state['instrument'] = st.sidebar.data_editor(st.session_state['instrument'].set_index('Attribute'), use_container_width=True)
 
   #settlement_parameters = pd.DataFrame({'Attribute':['Moneyness','Intrinsic value','Is in the money'],'Value':[f'{moneyess_perc}%',intrinsic,inthemoney]})
   #st.session_state['settlement_parameters'] = st.sidebar.data_editor(settlement_parameters,hide_index=True, disabled=(['Attribute','Value']), use_container_width=True)
