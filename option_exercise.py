6f6c97433ca34bc9ddba8b0b9b0528a4b1626410
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-11-08 12:49:42 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -136,7 +136,7 @@ if st.session_state['example'] == "Single instrument":
   st.markdown("<p style='text-align: center; margin-top: 5px; margin-bottom: 5px;'font-size:16px;", unsafe_allow_html=True)
   
   if ifexercise:
-      broker_pos_table = st.table(st.session_state['broker_pos'].set_index('Account'))
+      st.session_state['broker_pos']
 
     
     ### SETTLEMENT FLOWS ###
