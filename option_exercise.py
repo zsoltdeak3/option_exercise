7f13d6fb6dce7d72f312a826605a3cc9605906f2
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-11-08 13:21:38 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -36,7 +36,7 @@ if st.session_state['example'] == "Single instrument":
   st.sidebar.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Option Attributes</p>", unsafe_allow_html=True)
   instrument = {'Attribute': ['Symbol', 'Type', 'Contract size', 'Strike', 'Underlying', 'EDSP'],
                   'Value': ['Opt1', 'Call', 1000, 500, 'Und1', 1200]}
-  instrument.set_index('Attribute',inplace=True)
+  instrument = instrument.set_index('Attribute')
   st.session_state['instrument'] = st.sidebar.data_editor(instrument, use_container_width=True)
 
   #settlement_parameters = pd.DataFrame({'Attribute':['Moneyness','Intrinsic value','Is in the money'],'Value':[f'{moneyess_perc}%',intrinsic,inthemoney]})
