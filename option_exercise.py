0428753d204c7f72a8c28963fef6ac36f82e771e
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-11-08 13:20:39 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -34,11 +34,10 @@ if st.session_state['example'] == "Single instrument":
 
   ###Editable option attributes###
   st.sidebar.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Option Attributes</p>", unsafe_allow_html=True)
-  if 'instrument' not in st.session_state:
-    instrument = {'Attribute': ['Symbol', 'Type', 'Contract size', 'Strike', 'Underlying', 'EDSP'],
+  instrument = {'Attribute': ['Symbol', 'Type', 'Contract size', 'Strike', 'Underlying', 'EDSP'],
                   'Value': ['Opt1', 'Call', 1000, 500, 'Und1', 1200]}
-    st.session_state['instrument'] = pd.DataFrame(instrument)
-  st.session_state['instrument'] = st.sidebar.data_editor(st.session_state['instrument'].set_index('Attribute'), use_container_width=True)
+  instrument.set_index('Attribute',inplace=True)
+  st.session_state['instrument'] = st.sidebar.data_editor(instrument, use_container_width=True)
 
   #settlement_parameters = pd.DataFrame({'Attribute':['Moneyness','Intrinsic value','Is in the money'],'Value':[f'{moneyess_perc}%',intrinsic,inthemoney]})
   #st.session_state['settlement_parameters'] = st.sidebar.data_editor(settlement_parameters,hide_index=True, disabled=(['Attribute','Value']), use_container_width=True)
