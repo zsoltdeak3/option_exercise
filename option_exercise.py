21e8c46ac4f067a09a0f26590c2c61326c408b3b
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-11-08 13:22:36 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -34,8 +34,8 @@ if st.session_state['example'] == "Single instrument":
 
   ###Editable option attributes###
   st.sidebar.markdown("<p style='text-align: center; margin-bottom: -10px;'font-size:18px;'>Option Attributes</p>", unsafe_allow_html=True)
-  instrument = {'Attribute': ['Symbol', 'Type', 'Contract size', 'Strike', 'Underlying', 'EDSP'],
-                  'Value': ['Opt1', 'Call', 1000, 500, 'Und1', 1200]}
+  instrument = pd.DataFrame({'Attribute': ['Symbol', 'Type', 'Contract size', 'Strike', 'Underlying', 'EDSP'],
+                  'Value': ['Opt1', 'Call', 1000, 500, 'Und1', 1200]})
   instrument = instrument.set_index('Attribute')
   st.session_state['instrument'] = st.sidebar.data_editor(instrument, use_container_width=True)
 
