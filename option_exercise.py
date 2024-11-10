698f2f3a9a9e9edafc8bf14f83567693ea4ec14f
zsoltdeak3 <zsolt.deak3@gmail.com>
2024-11-10 11:24:54 +0300

    Update option_exercise.py

--- option_exercise.py
+++ option_exercise.py
@@ -132,9 +132,10 @@ if st.session_state['example'] == "Single instrument":
         <b>Step 4:</b> Determine whether to exercise options. </div>
     """,unsafe_allow_html=True)
     #Empty line
-  moneyness_df = st.session_state['broker_pos'].set_index('Symbol').join(st.session_state['instruments'][['Strike', 'Contract size', 'Underlying']], how='left')
+  moneyness_df = st.session_state['broker_pos'].set_index('Symbol').join(st.session_state['instruments'][['Strike', 'Contract size', 'Underlying', 'Type']], how='left')
   moneyness_df = moneyness_df.set_index('Underlying', drop=False).join(st.session_state['edsp_df'][['EDSP']], how='left')
-  moneyness_df ['Moneyness'] = np.divide(moneyness_df['Strike'],moneyness_df['EDSP'])
+  moneyness_df ['Moneyness'] = np.where(moneyness_df['Strike'] == 'Put', np.divide(moneyness_df['Strike'],moneyness_df['EDSP']),
+                                         np.divide(moneyness_df['Strike'],moneyness_df['EDSP']))
   moneyness_df = moneyness_df.reset_index(drop=True)
   st.dataframe(moneyness_df, hide_index=True)
   ifexercise = st.button(label='Decide if exercise')  
